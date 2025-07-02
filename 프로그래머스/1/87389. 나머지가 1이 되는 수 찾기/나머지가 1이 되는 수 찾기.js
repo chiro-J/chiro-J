53eefe2b02bb2a948c5dbbd3396b795ad06086e2
Cheolho Jang <103028942+chiro-J@users.noreply.github.com>
2025-07-02 10:26:56 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.29 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    return dividor(n);
+}
+
+function dividor (n) {
+    for (let i = 1; i < n; i++) {
+        if (n % i == 1) {
+            return i;
+        }
+    }
+}
+
+// n을 x로 나눈 나머지가 1
+// (n - 1) % x = 0
