28cf94cdacfb99066795876c7179751e467451ed
chiro-J <wkdcjfgh0222.gmail.com>
2025-07-28 18:00:22 +0900

    0728 수업 내용 커밋

---
+++ study/0728/ch07_3_server/src/test/connectTest.ts
@@ -0,0 +1,12 @@
+import { connect } from 'http2'
+import * as M from '../mongodb'
+
+const connectCB = (db: M.MongoDB) => {
+    console.log('db', db)
+}
+
+const connectTest = () => {
+    M.connectAndUseDB(connectCB, 'ch07')
+}
+
+connectTest()
